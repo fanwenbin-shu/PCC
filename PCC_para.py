bbfa26b54b0eb3aeaf58b591c146ee625d4e97cc
Wenbin, FAN <fanwenbin@shu.edu.cn>
2021-06-20 20:47:03 +0800

    initial

---
+++ PCC_para.py
@@ -0,0 +1,14 @@
+# number of images in each axis
+Nim = [1, 0, 0]
+# charge, for Gaussian calculation
+charge = 9
+# spin multiplicity
+sm = 1
+# Gaussian route card
+gau_memory = 4 # unit : GB
+gau_nprocs = 8
+# space group
+space_group = 18
+
+# convergence criteria
+max_diff_crt = 1e-5
